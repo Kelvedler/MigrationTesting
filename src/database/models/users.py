0db9597619c306604243043ad9ef4b1be85b9494
Kelvedler <hnflkmvjcm@gmail.com>
2024-12-09 12:08:43 +0200

    add active flag

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import Boolean, String, text
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
@@ -9,3 +9,6 @@ class Users(Base):
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(250), nullable=False)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean(), nullable=False, server_default=text("1")
+    )
